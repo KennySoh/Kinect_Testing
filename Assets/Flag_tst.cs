1d1aafeb13d1c0b24a1c522f3e05af2ccb736e97
Joe627487136 <joe627487136@hotmail.com>
2017-09-29 11:47:33 +0800

    Untiy Testing

---
+++ Assets/Flag_tst.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Flag_tst : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+		print ("Enter");
+	}
+
+	void OnTriggerStay(Collider col) {
+		print ("Stay");
+	}
+
+	void OnTriggerExit(Collider col) {
+		print ("Exit");
+	}
+}
---
+++ Assets/scripts/Flag_Check.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Flag_Check : MonoBehaviour {
+	void OnTriggerEnter(Collider col) {
+		GameObject.FindGameObjectWithTag ("test_tag").GetComponent<Text> ().text = "Hand in!";
+		print ("Enter");
+	}
+
+	void OnTriggerExit(Collider col) {
+		GameObject.FindGameObjectWithTag ("test_tag").GetComponent<Text> ().text = "Hand out!";
+		print ("Exit");
+	}
+}
